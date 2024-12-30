f0910d93a51d8e9ffc1fcc45f6aa130be869fbf2
Frederick Asante <Rastinga1995@hotmail.com>
2024-12-30 06:12:28 +0000

    Add project files.

---
+++ BlazorHybridInternetConnectivity/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace BlazorHybridInternetConnectivity
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+           
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
